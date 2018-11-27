07814ba3b716f74409e1ea9a2a262d79ae512e4e
Lailatul Khusna <lailatulkhusna@students.unnes.ac.id>
2018-11-28 02:45:47 +0700

    update

--- app/Http/Controllers/API/FieldListController.php
+++ app/Http/Controllers/API/FieldListController.php
@@ -42,7 +42,7 @@ class FieldListController extends Controller
      */
     public function show($id)
     {
-        $fieldlist=FieldList::with('branch','question_list','field')->find($id)->get();
+        $fieldlist=FieldList::with('branch','question_list','field')->find($id);
         
         return response()->json($fieldlist);
     }
