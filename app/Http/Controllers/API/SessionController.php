e8c9fb44cbbf8f40d6db30a744d781af1ae42c00
Lailatul Khusna <lailatulkhusna@students.unnes.ac.id>
2018-11-28 23:54:28 +0700

    update

--- app/Http/Controllers/API/SessionController.php
+++ app/Http/Controllers/API/SessionController.php
@@ -28,7 +28,7 @@ class SessionController extends Controller
     public function store(Request $request)
     {
         // return response()->json($request);
-        $count = Session::where('branch_id',$request['session']['branch_id
+        $count = Session::where('branch_id',$request['user']['branch_id
             '])->count();
         $session = new Session;
         $session->branch_id = $request['session']['branch_id'];
