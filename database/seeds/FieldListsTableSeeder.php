22e030ebce9ff797346735180a5b472cf750ba53
Lailatul Khusna <lailatulkhusna@students.unnes.ac.id>
2019-01-27 20:05:58 +0700

    fieldlist table seeder

--- database/seeds/FieldListsTableSeeder.php
+++ database/seeds/FieldListsTableSeeder.php
@@ -13,12 +13,25 @@ class FieldListsTableSeeder extends Seeder
     {
          $data=[[
         	'branch_id' => 1,
-        	'name' => 'Kebersihan',
-        	'description' => 'tentang kebersihan bengkel'
+        	'name' => 'Bukti Fisik (Tangible)',
+        	'description' => 'Bukti  secara fisik   yaitu  bukti   yang  ditunjukkan  oleh fasilitas  fisik & peralatan  yang  digunakan,'
         ],[
-        	'branch_id' => 2,
-        	'name' => 'Kedisiplinan',
-        	'description' => 'tentang kedisplinan bengkel'
+        	'branch_id' => 1,
+        	'name' => 'Keandalan (Reliability)',
+        	'description' => 'Menyajikan jasa  sesuai  dengan  janji  serta  akurat  dan memuaskan'
+        ],[
+            'branch_id' => 1,
+            'name' => 'Daya Tanggap (Responsiveness)',
+            'description' => 'Ketersediaan para karyawan untuk membantu  pelanggan  dan  menyajikan  jasa dengan segera'
+        ],[
+            'branch_id' => 1,
+            'name' => 'Jaminan (Assurance)',
+            'description' => 'Pengetahuan, ketrampilan  dan  kemampuan dalam  menyajikan  jasa'
+        ],[
+            'branch_id' => 1,
+            'name' => 'Empati (Empaty)',
+            'description' => 'kemudahan     dalam berinteraksi, komunikasi yang baik, memberikan  perhatian  secara  pribadi  serta memahami     kebutuhandan     keinginan pelanggan'
+
         ]];
 
         DB::table('field_lists')->insert($data);
